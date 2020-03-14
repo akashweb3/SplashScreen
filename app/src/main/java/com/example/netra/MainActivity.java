60057199507dbb461b3aa26ea97987545d4d9364
Akahs Prajapati <akashweb3@gmail.com>
2020-03-14 08:40:25 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/netra/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.netra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+
+    //variables
+    Animation topAnim, bottomAnim;
+    GifImageView image;
+    TextView logo, slogan;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Animations
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+        image = findViewById(R.id.gifImageView);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, FaceActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, SPLASH_SCREEN);
+    }
+}
